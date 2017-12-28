7e6007e7c38e4b8ae223e0d34f002b81c9901b43
Alexey <alexone3@list.ru>
2017-12-29 00:12:17 +0200

    Update ProductController.java

--- src/main/java/com/shop/mvc/controllers/ProductController.java
+++ src/main/java/com/shop/mvc/controllers/ProductController.java
@@ -14,6 +14,38 @@ import java.util.Objects;
 @Controller
 public class ProductController {
 
+   @RequestMapping(value = "/admin/addLaptop", method = RequestMethod.GET)
+    public String addLaptop(HttpSession session, Model model){
+        UserDto admin = (UserDto) session.getAttribute("admin");
+        if(Objects.isNull(admin)){
+            return "redirect:/";
+        }
+        if(!model.containsAttribute("laptop")){
+            model.addAttribute("laptop",new ComputerDto());
+        }
+        return "addLaptopPage";
+    }
+
+    @RequestMapping(value = "/admin/addMobilePhone", method = RequestMethod.GET)
+    public ModelAndView addMobilePhone(Model model){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("addMobilePhonePage");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/admin/addRacket", method = RequestMethod.GET)
+    public ModelAndView addRacket(Model model){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("addRacketPage");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/admin/addBall", method = RequestMethod.GET)
+    public ModelAndView addBall(Model model){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("addBallPage");
+        return modelAndView;
+    }
 
 
 }
